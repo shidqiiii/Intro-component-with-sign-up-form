f2d2b66b04b13d0d25eec838cb601bc591b9f789
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-05-16 20:54:59 +0700

    update error message in form

--- js/main.js
+++ js/main.js
@@ -1,14 +1,37 @@
 let button = document.getElementById("claim-free");
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-button.addEventListener('click', function(e) {
+let form = document.getElementById("form-input");
+
+form.addEventListener('submit', function(e) {
     e.preventDefault();
-    window.location.reload();
+    // window.location.reload();
+
+    if (firstName.value === "") {
+        setError(firstName, "First Name cannot be empty")
+    } else {
+        firstName.parentElement.classList.remove("error")
+    }
+
+    if (lastName.value === "") {
+        setError(lastName, "Last Name cannot be empty")
+    } else {
+        lastName.parentElement.classList.remove("error")
+    }
 })
 
 function checkInput() {
 
 }
 
-function setError() {
+function setError(input, message) {
+    const formInput = input.parentElement;
+    const label = formInput.querySelector("label");
 
+    label.innerText = message;
+
+    formInput.classList.add("error");
 }
