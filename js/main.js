6f019745a8e7ebc37af69b2bff741d0b1e35518e
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-05-16 20:17:57 +0700

    update html, css, js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("claim-free");
+
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    window.location.reload();
+})
+
+function checkInput() {
+
+}
+
+function setError() {
+
+}
